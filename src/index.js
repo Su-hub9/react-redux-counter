8e1f42bb0989b1d9382f915768ae8454b02c9581
suji.bae <suji.bae@datastreams.co.kr>
2021-06-16 14:30:59 +0900

    1장 카운터 만들기

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './containers/App';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// Redux 관련 객체 불러오기
+import { createStore } from "redux";
+import reducer from './reducers';
+import { Provider } from "react-redux";
+
+// 스토어 생성
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
